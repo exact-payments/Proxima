b4a0c3de4cc598d641760d5f4cb6eaa10c6a9975
Robert Hurst <rhurst@e-xact.com>
2017-05-09 13:45:16 -0700

    Expose responses

--- lib/proxima/model.rb
+++ lib/proxima/model.rb
@@ -32,41 +32,59 @@ module Proxima
       @api
     end
 
+    def self.response
+      @response
+    end
+
+    def self.responses
+      @responses || []
+    end
+
     def self.create(record)
-      return record.map { |record| self.create record } if record.is_a? Array
-      model = self.new(record)
-      return nil unless model.save
+      if record.is_a? Array
+        models     = []
+        @responses = []
+        record.each do |record|
+          model = self.create record
+          @responses.push(model.response)
+          models.push(model) if model
+        end
+        return models
+      end
+
+      model     = self.new(record)
+      save_ok   = model.save
+      @response = model.response
+
+      return nil unless save_ok
       model
     end
 
     def self.find(query = {}, opts = {})
       opts[:query] = self.convert_query_or_delta_to_json query
-      response     = self.api.get self.find_path.call(query), opts
+      @response    = self.api.get self.find_path.call(query), opts
 
-      return [] unless response.code == 200
-
-      records = ActiveSupport::JSON.decode response.body
-      records.map do |record|
-        model = self.new
-        model.from_json record
-        model.new_record = false
-        model
+      if @response.code != 200
+        return []
       end
+
+      models = self.from_json @response.body
+      models.each { |model| model.new_record = false }
     end
 
     def self.find_one(query = {}, opts = {})
-      query['$limit'] = 1;
+      query['$limit'] = 1
       self.find(query, opts)[0]
     end
 
     def self.count(query = {}, opts = {})
       query['$limit'] = 0
       opts[:query]    = self.convert_query_or_delta_to_json query
-      response        = self.api.get self.find_path.call(query), opts
+      @response       = self.api.get self.find_path.call(query), opts
 
-      return nil unless response.code == 200
+      return nil unless @response.code == 200
 
-      response.headers[:x_total_count] || 0
+      @response.headers[:x_total_count] || 0
     end
 
     def self.find_by_id(id, query = {}, opts = {})
@@ -76,12 +94,12 @@ module Proxima
       end
 
       query[:id] = id
-      response = self.api.get self.find_by_id_path.call(query), opts
+      @response  = self.api.get self.find_by_id_path.call(query), opts
 
-      return nil unless response.code == 200
+      return nil unless @response.code == 200
 
       model = self.new
-      model.from_json response.body
+      model.from_json @response.body
       model.new_record = false
       model
     end
@@ -110,17 +128,21 @@ module Proxima
       clear_changes_information unless val
     end
 
+    def response
+      @response
+    end
+
     def save(options = {})
       return false unless self.valid?
 
       if self.new_record?
-        path     = self.class.create_path.call self.to_h
-        payload  = { json: self.as_json(options) }
-        response = self.class.api.post path, payload
+        path      = self.class.create_path.call self.to_h
+        payload   = { json: self.as_json(options) }
+        @response = self.class.api.post path, payload
 
-        return false unless response.code == 201
+        return false unless @response.code == 201
 
-        self.from_json response.body, options[:include_root]
+        self.from_json @response.body, options[:include_root]
         self.new_record = false
         return true
       end
@@ -128,11 +150,11 @@ module Proxima
       return true if self.persisted?
 
       options[:flatten] = true if options[:flatten] == nil
-      path     = self.class.update_by_id_path.call self.to_h
-      payload  = { json: self.as_json(options) }
-      response = self.class.api.put path, payload
+      path      = self.class.update_by_id_path.call self.to_h
+      payload   = { json: self.as_json(options) }
+      @response = self.class.api.put path, payload
 
-      return false unless response.code == 204
+      return false unless @response.code == 204
       self.persisted = true
     end
 
@@ -147,18 +169,18 @@ module Proxima
     def destroy
       return false if new_record?
 
-      response = self.class.api.delete(self.class.delete_by_id_path.call(self.to_h))
+      @response = self.class.api.delete(self.class.delete_by_id_path.call(self.to_h))
 
-      return false unless response.code == 204
+      return false unless @response.code == 204
       self.persisted = true
     end
 
     def restore
       return false if new_record?
 
-      response = self.class.api.post(self.class.restore_by_id_path.call(self.to_h))
+      @response = self.class.api.post(self.class.restore_by_id_path.call(self.to_h))
 
-      return false unless response.code == 204
+      return false unless @response.code == 204
       self.persisted = true
     end
   end
